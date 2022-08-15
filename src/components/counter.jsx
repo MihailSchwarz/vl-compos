347d85c641aa279b5cd9de97ce3f1f112d9ef216
MihailSchwarz <mihawin@ya.ru>
2022-08-15 21:34:30 +0100

    =

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,61 +1,46 @@
 import React, { useState } from "react";
-const Counter = () => {
-    const [count, setCount] = useState(0);
-    const [tags, setTags] = useState(["tag1", "tag2", "tag3"]);
+const Counter = (props) => {
+  console.log(props);
+  //const [value, setValue] = useState(props.value);
+  //назначаем значения
+  const { value } = props;
 
-    const formatCount = () => {
-        return count === 0 ? "empty" : count;
-    };
-    const getBageClasses = () => {
-        let classes = "badge m-2 ";
-        classes += count === 0 ? "bg-warning" : "bg-primary";
-        return classes;
-    };
+  const formatValue = () => {
+    return value === 0 ? "empty" : value;
+  };
+  const getBageClasses = () => {
+    let classes = "badge m-2 ";
+    classes += value === 0 ? "bg-warning" : "bg-primary";
+    return classes;
+  };
 
-    const handleIncrement = () => {
-        setCount((prevState) => prevState + 1);
-    };
-    const handleDecrement = () => {
-        setCount((prevState) => prevState - 1);
-    };
-    const handleTagChange = (id) => {
-        setTags((prevState) => prevState.filter((tag) => tag !== id));
-    };
-    const renderTags = () => {
-        return (
-            tags.length !== 0 &&
-            tags.map((tag) => (
-                <li
-                    key={tag}
-                    className='btn btn-primary btn-sm m-2'
-                    onClick={() => handleTagChange(tag)}
-                >
-                    {tag}
-                </li>
-            ))
-        );
-    };
-    if (tags.length !== 0) {
-        return <ul>{renderTags()}</ul>;
-    }
+  const handleIncrement = () => {
+    //setValue((prevState) => prevState + 1);
+    console.log("hadle Increment");
+  };
+  const handleDecrement = () => {
+    //setValue((prevState) => prevState - 1);
+    console.log("hadle Decrement");
+  };
 
-    return (
-        <>
-            <span className={getBageClasses()}>{formatCount()}</span>
-            <button
-                className='btn btn-primary btn-sm m-2'
-                onClick={handleIncrement}
-            >
-                +
-            </button>
-            <button
-                className='btn btn-primary btn-sm m-2'
-                onClick={handleDecrement}
-            >
-                -
-            </button>
-        </>
-    );
+  return (
+    <div>
+      <span>{props.name}</span>
+      <span className={getBageClasses()}>{formatValue()}</span>
+      <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>
+        +
+      </button>
+      <button className="btn btn-primary btn-sm m-2" onClick={handleDecrement}>
+        -
+      </button>
+      <button
+        className="btn btn-danger btn-sm m2"
+        onClick={() => props.onDelete(props.id)}
+      >
+        Delete
+      </button>
+    </div>
+  );
 };
 
 export default Counter;
